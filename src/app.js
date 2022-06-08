6e3a1c417bd56cbc9164ec971588a4fe5722f814
Samuel Palacios <samuelpl0888@gmail.com>
2022-06-08 18:24:01 -0400

    Create js script

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  
+  let button = document.querySelector('#btn');
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
+  button.addEventListener('click', () => {
+    let excuse = document.querySelector('#the-excuse');
+    excuse.innerHTML = generateExcuse();
+  })
   console.log("Hello Rigo from the console!");
 };
+
+let generateExcuse = () => {
+  let subject = ['My friend', 'The dog', 'A bird', 'Her turtle'];
+  let action = ['ate my', 'broke my', 'stole my', 'took my'];
+  let what = ['keys', 'homework', 'shoe', 'car', 'book'];
+  let where = ['on the street', 'in the office', 'in class', 'in the house']; 
+
+  let subjIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whereIndex =Math.floor(Math.random() * where.length);
+
+  return `${subject[subjIndex]} ${action[actionIndex]} ${what[whatIndex]} ${where[whereIndex]}`;
+}
